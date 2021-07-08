d18350d6db7558e3542f6296a2ff94042bb60de4
jefersondeff <jefersondefariasferreira@gmail.com>
2021-07-08 10:22:42 -0300

    feat: adiciona capitulo dois

---
+++ chapter_two/src/main.rs
@@ -0,0 +1,35 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Advinhe o número!");
+
+    let numero_secreto = rand::thread_rng().gen_range(1, 10);
+
+    loop {
+        println!("Digite o seu palpite.");
+
+        let mut palpite = String::new();
+        io::stdin()
+            .read_line(&mut palpite)
+            .expect("Falha ao ler entrada");
+        println!("Você disse: {}", palpite);
+
+        let palpite: u32 = match palpite.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match palpite.cmp(&numero_secreto) {
+            Ordering::Less => println!("Muito baixo!"),
+            Ordering::Greater => println!("Muito alto!"),
+            Ordering::Equal => {
+                println!("Você acertou!");
+                break;
+            }
+        }
+    }
+}
